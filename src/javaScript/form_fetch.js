94ae949d14d4c4d9e223565c367611195400ebbd
edwinjc01 <90839652+edwinjc01@users.noreply.github.com>
2023-02-10 17:08:48 +0100

    Har fixat satt man inte kan regestrera sig om man inte har lagt till en profilbild

--- src/javaScript/form_fetch.js
+++ src/javaScript/form_fetch.js
@@ -1,95 +1,137 @@
+let url1 = ""
+let url2 = ""
+let url3 = ""
+let url4 = ""
+let url5 = ""
 
-
- async function createUser (){
-
-
-
-
-
-
-     const container = document.getElementById("containerBild");
-     const images = container.querySelectorAll(".foto img");
-
-     let resSports = "";
-     let resPronomen = "";
-     let checkboxes = document.getElementsByClassName('sportCheck');
+ async function createUser () {
 
 
 
-     for (let i = 0; i < checkboxes.length; i++) {
-         if (checkboxes[i].checked) {
-             let idTo = checkboxes[i].id + "rangeValue";
-             idTo = idTo[0].toUpperCase() + idTo.slice(1);
-             let check = document.getElementById(idTo).textContent;
-             resSports += " | "+ checkboxes[i].value + " " + check;
-         }
-     }
-
-     let pronomen = document.getElementsByClassName('pronomen');
-
-     for (let i = 0; i < pronomen.length; i++) {
-         if (pronomen[i].checked) {
-             resPronomen += " | " + pronomen[i].value;
-         }
-     }
-
-    const data = {
+     const image = document.querySelector("#containerBild > :first-child img").src;
 
 
-        username: document.getElementById("användarnamn").value,
-        email: document.getElementById("email").value,
-        password: document.getElementById("lösenord2").value,
-        firstname: document.getElementById("förnamn").value,
-        lastname: document.getElementById("efternamn").value,
-        doB:
-             document.getElementById('år').value + '-' +
-            document.getElementById('månad').value + '-' +
-            document.getElementById('dag').value,
-
-        city: document.getElementById("stad").value,
-        bio: document.getElementById('myTextarea').value,
-        interests: resSports,
-
-
-
-        gender: document.querySelector('input[name="kön"]:checked').value,
-        genderPreference: resPronomen,
-
-        imageUrlsDto: {
-            mainImg: images[0].src
-        }
-
-
-    }
-
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-
-    const url = "http://localhost:8080/register/";
-
-
-     const createUserRequest = new Request (url, {
-         method: 'POST',
-         redirect: 'follow',
-         body: JSON.stringify(data),
-         headers: headers,
-         cache: "default",
- });
-
-
-    await fetch(createUserRequest)
-         .then((response) => {
-             if(!response.ok){
-                 alert("Det går icke")
-                 throw new Error("BAJSKORV")
+     let container = document.getElementById("containerBild");
+     let children = container.children;
+     let urlCounter = 1;
+     for (let i = 1; i < children.length; i++) {
+         let child = children[i].querySelector("img");
+         if (child != null) {
+             let src = child.src;
+             switch (urlCounter) {
+                 case 1:
+                     url1 = src;
+                     break;
+                 case 2:
+                     url2 = src;
+                     break;
+                 case 3:
+                     url3 = src;
+                     break;
+                 case 4:
+                     url4 = src;
+                     break;
+                 case 5:
+                     url5 = src;
+                     break;
              }
-             return response.json();
-         })
-         .then((response) =>{
-             console.log(response)
-         })
-  /*        .catch((error) => {
-             console.error('Error ', error);
-         });*/
-}
+             urlCounter++;
 
+         }
+     }
+
+     let genus = document.querySelector('input[name="kön"]:checked').value
+
+     if (genus === "ANPASSAT"){
+         genus += "| " + document.getElementById("selectKön").value + "| " + document.getElementById("textKön").value;
+     }
+
+
+         let resSports = "";
+         let resPronomen = "";
+         let checkboxes = document.getElementsByClassName('sportCheck');
+
+
+         for (let i = 0; i < checkboxes.length; i++) {
+             if (checkboxes[i].checked) {
+                 let idTo = checkboxes[i].id + "rangeValue";
+                 idTo = idTo[0].toUpperCase() + idTo.slice(1);
+                 let check = document.getElementById(idTo).textContent;
+                 resSports += " | " + checkboxes[i].value + " " + check;
+             }
+         }
+
+         let pronomen = document.getElementsByClassName('pronomen');
+
+         for (let i = 0; i < pronomen.length; i++) {
+             if (pronomen[i].checked) {
+                 resPronomen += " | " + pronomen[i].value;
+             }
+         }
+
+
+         const data = {
+
+
+             username: document.getElementById("användarnamn").value,
+             email: document.getElementById("email").value,
+             password: document.getElementById("lösenord2").value,
+             firstname: document.getElementById("förnamn").value,
+             lastname: document.getElementById("efternamn").value,
+             doB:
+                 document.getElementById('år').value + '-' +
+                 document.getElementById('månad').value + '-' +
+                 document.getElementById('dag').value,
+
+             city: document.getElementById("stad").value,
+             bio: document.getElementById('myTextarea').value,
+             interests: resSports,
+
+
+             gender: genus,
+             genderPreference: resPronomen,
+
+             imageUrlsDto: {
+                 mainImg: image,
+                 url1: url1,
+                 url2: url2,
+                 url3: url3,
+                 url4: url4,
+                 url5: url5
+             }
+
+
+         }
+
+         const headers = new Headers();
+         headers.append('Content-Type', 'application/json');
+
+         const url = "http://localhost:8080/register/";
+
+
+         const createUserRequest = new Request(url, {
+             method: 'POST',
+             redirect: 'follow',
+             body: JSON.stringify(data),
+             headers: headers,
+             cache: "default",
+         });
+
+
+         await fetch(createUserRequest)
+             .then((response) => {
+                 if (!response.ok) {
+                     alert("Det går icke")
+                     throw new Error("BAJSKORV")
+                 }
+                 return response.json();
+             })
+             .then((response) => {
+                 console.log(response)
+             })
+         /*        .catch((error) => {
+                    console.error('Error ', error);
+                });*/
+
+
+ }
