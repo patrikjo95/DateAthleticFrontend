242e38a2060606a2966fc012d88b4220b6cf4514
Bryan Bautista <bryanalberto31@gmail.com>
2023-02-13 15:46:31 +0100

    nice

--- src/javaScript/profil.js
+++ src/javaScript/profil.js
@@ -1,6 +1,9 @@
 let dropdownUl = document.querySelector(".dropdown-ul");
 let btn = document.querySelector(".profil-btn");
 
+let token = localStorage.getItem("token")
+console.log(token);
+
 
 
 const toggle = () => dropdownUl.classList.toggle("active");
@@ -18,3 +21,26 @@ function showPreview(event){
     preview.style.display = "block";
   }
 }
+
+const name = document.getElementById("name")
+const username = document.getElementById("username")
+const bio = document.getElementById("bio")
+const profilePic = document.getElementById("profilePic")
+
+fetch('http://localhost:8080/user/profile/', {
+  headers:{
+    'Authorization': 'Bearer ' + token,
+  }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+       var rawInfo = JSON.stringify(data)
+       var info = JSON.parse(rawInfo)
+       console.log(info);
+       name.textContent = data.firstname
+       username.textContent = data.username
+       bio.textContent = data.bio
+       
+       
+       profilePic.src = "/" + info.imagePath[0]
+      })
