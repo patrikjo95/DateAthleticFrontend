15ba07fcc4540c97abdf3a97fc7c52b4bb968287
Bryan Bautista <bryanalberto31@gmail.com>
2023-02-15 22:21:10 +0100

    Fetch attempts

--- src/javaScript/profil.js
+++ src/javaScript/profil.js
@@ -5,6 +5,10 @@ let token = localStorage.getItem("token")
 console.log(token);
 
 
+const name = document.getElementById("name")
+const username = document.getElementById("username")
+const bio = document.getElementById("bio")
+const profilePic = document.getElementById("profilePic")
 
 const toggle = () => dropdownUl.classList.toggle("active");
 
@@ -18,14 +22,9 @@ function showPreview(event){
     var src = URL.createObjectURL(event.target.files[0]);
     var preview = document.getElementById("file-ip-1-preview");
     preview.src = src;
-    preview.style.display = "block";
+    preview.style.display = "block";  
   }
 }
-
-const name = document.getElementById("name")
-const username = document.getElementById("username")
-const bio = document.getElementById("bio")
-const profilePic = document.getElementById("profilePic")
 
 
 fetch('http://localhost:8080/user/profile/', {
@@ -41,6 +40,13 @@ fetch('http://localhost:8080/user/profile/', {
        name.textContent = data.firstname
        username.textContent = data.username
        bio.textContent = data.bio       
-       
        profilePic.src = "/" + info.imagePath[0]
+
+       //document.getElementById("firstname").value = "121212"
+       document.getElementById("lastname").value = data.lastname
+       document.getElementById("email").value = data.email
+       document.getElementById("city").value = data.city
+       document.getElementsByName('förnamn')[0].placeholder="asdfasdf"
+       //document.getElementById('data').setAttribute("placeholder", "placeholder value");
+
       })
